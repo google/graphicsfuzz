c1f9de902aa1ad79c46722be130e6cb135eaf92b
Paul Thomson <paulthomson@google.com>
2018-10-03 15:27:24 +0100

    Fix 1-install-deps-travis.sh

--- build/travis/1-install-deps-travis.sh
+++ build/travis/1-install-deps-travis.sh
@@ -18,6 +18,6 @@ set -x
 set -e
 set -u
 
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get install python3.6
+sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+sudo apt-get update -q
+sudo apt-get install python3.6 -y
