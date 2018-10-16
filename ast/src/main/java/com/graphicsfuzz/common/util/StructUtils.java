c2e1115cfe2bb0bf2f867ddc2b89af729b8df394
Alastair Donaldson <afdx@google.com>
2018-10-16 10:29:22 +0100

    Added copyright headers.

--- ast/src/main/java/com/graphicsfuzz/common/util/StructUtils.java
+++ ast/src/main/java/com/graphicsfuzz/common/util/StructUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 The GraphicsFuzz Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.graphicsfuzz.common.util;
 
 import com.graphicsfuzz.common.ast.IAstNode;
@@ -7,7 +23,6 @@ import com.graphicsfuzz.common.ast.expr.TypeConstructorExpr;
 import com.graphicsfuzz.common.ast.type.StructDefinitionType;
 import com.graphicsfuzz.common.ast.type.StructNameType;
 import com.graphicsfuzz.common.ast.visitors.StandardVisitor;
-import com.graphicsfuzz.common.typing.ScopeTreeBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
