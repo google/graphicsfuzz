f1cbad0a726ca4049afe7a2e2d185220b33e2c6b
Alastair Donaldson <afdx@google.com>
2019-01-24 13:24:15 +0000

    Added support for zero-setting of matrix uniforms. (#220)

--- common/src/test/java/com/graphicsfuzz/common/util/UniformsInfoTest.java
+++ common/src/test/java/com/graphicsfuzz/common/util/UniformsInfoTest.java
@@ -104,5 +104,173 @@ public class UniformsInfoTest {
     assertEquals(0, uniformsInfo.getBinding("a"));
   }
 
+  @Test
+  public void testZeroUnsetMatrices() throws Exception {
+    final String expectedUniforms = "{" +
+        "  \"m1\": {" +
+        "    \"func\": \"glUniformMatrix2fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m2\": {" +
+        "    \"func\": \"glUniformMatrix2x3fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m3\": {" +
+        "    \"func\": \"glUniformMatrix2x4fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m4\": {" +
+        "    \"func\": \"glUniformMatrix3x2fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m5\": {" +
+        "    \"func\": \"glUniformMatrix3fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m6\": {" +
+        "    \"func\": \"glUniformMatrix3x4fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }," +
+        "  \"m7\": {" +
+        "    \"func\": \"glUniformMatrix4x2fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }," +
+        "  \"m8\": {" +
+        "    \"func\": \"glUniformMatrix4x3fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }, " +
+        "  \"m9\": {" +
+        "    \"func\": \"glUniformMatrix4fv\"," +
+        "    \"args\": [" +
+        "      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" +
+        "    ]" +
+        "  }" +
+        "}";
+    final String shader = "#version 310 es\n"
+        + "uniform mat2x2 m1;"
+        + "uniform mat2x3 m2;"
+        + "uniform mat2x4 m3;"
+        + "uniform mat3x2 m4;"
+        + "uniform mat3x3 m5;"
+        + "uniform mat3x4 m6;"
+        + "uniform mat4x2 m7;"
+        + "uniform mat4x3 m8;"
+        + "uniform mat4x4 m9;"
+        + "void main() { }";
+    final UniformsInfo uniforms = new UniformsInfo("{}");
+    uniforms.zeroUnsetUniforms(ParseHelper.parse(shader));
+    assertEquals(new UniformsInfo(expectedUniforms).toString(), uniforms.toString());
+  }
+
+  private String zeros(int num) {
+    String result = "";
+    for (int i = 0; i < num; i++) {
+      if (i > 0) {
+        result += ", ";
+      }
+      result += "0.0";
+    }
+    return result;
+  }
+
+  @Test
+  public void testZeroUnsetMatrixArrays() throws Exception {
+    final String expectedUniforms = "{" +
+        "  \"m1\": {" +
+        "    \"func\": \"glUniformMatrix2fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(2 * 2 * 1) +
+        "    ]," +
+        "  \"count\": 1" +
+        "  }, " +
+        "  \"m2\": {" +
+        "    \"func\": \"glUniformMatrix2x3fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(2 * 3 * 2) +
+        "    ]," +
+        "  \"count\": 2" +
+        "  }, " +
+        "  \"m3\": {" +
+        "    \"func\": \"glUniformMatrix2x4fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(2 * 4 * 3) +
+        "    ]," +
+        "  \"count\": 3" +
+        "  }, " +
+        "  \"m4\": {" +
+        "    \"func\": \"glUniformMatrix3x2fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(3 * 2 * 4) +
+        "    ]," +
+        "  \"count\": 4" +
+        "  }, " +
+        "  \"m5\": {" +
+        "    \"func\": \"glUniformMatrix3fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(3 * 3 * 5) +
+        "    ]," +
+        "  \"count\": 5" +
+        "  }, " +
+        "  \"m6\": {" +
+        "    \"func\": \"glUniformMatrix3x4fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(3 * 4 * 6) +
+        "    ]," +
+        "  \"count\": 6" +
+        "  }," +
+        "  \"m7\": {" +
+        "    \"func\": \"glUniformMatrix4x2fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(4 * 2 * 7) +
+        "    ]," +
+        "  \"count\": 7" +
+        "  }," +
+        "  \"m8\": {" +
+        "    \"func\": \"glUniformMatrix4x3fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(4 * 3 * 8) +
+        "    ]," +
+        "  \"count\": 8" +
+        "  }, " +
+        "  \"m9\": {" +
+        "    \"func\": \"glUniformMatrix4fv\"," +
+        "    \"args\": [" +
+        "      " + zeros(4 * 4 * 9) +
+        "    ]," +
+        "  \"count\": 9" +
+        "  }" +
+        "}";
+    final String shader = "#version 310 es\n"
+        + "uniform mat2x2 m1[1];"
+        + "uniform mat2x3 m2[2];"
+        + "uniform mat2x4 m3[3];"
+        + "uniform mat3x2 m4[4];"
+        + "uniform mat3x3 m5[5];"
+        + "uniform mat3x4 m6[6];"
+        + "uniform mat4x2 m7[7];"
+        + "uniform mat4x3 m8[8];"
+        + "uniform mat4x4 m9[9];"
+        + "void main() { }";
+    final UniformsInfo uniforms = new UniformsInfo("{}");
+    uniforms.zeroUnsetUniforms(ParseHelper.parse(shader));
+    assertEquals(new UniformsInfo(expectedUniforms).toString(), uniforms.toString());
+  }
+
 
 }
