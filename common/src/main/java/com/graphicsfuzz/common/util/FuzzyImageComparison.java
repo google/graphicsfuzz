66618b42ea041c9d9f1875deef6232a5e3e14bad
Paul Thomson <paulthomson@google.com>
2019-02-19 17:25:44 +0000

    Add comments, remove some unused code

--- common/src/main/java/com/graphicsfuzz/common/util/FuzzyImageComparison.java
+++ common/src/main/java/com/graphicsfuzz/common/util/FuzzyImageComparison.java
@@ -21,17 +21,57 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**
+ * A fuzzy image comparison algorithm.
+ *
+ * Inputs: two images (image A and image B) of the same size, a distance threshold "d", a component
+ * threshold "c".
+ * Output: the number of "bad" pixels that "differ" according to the description below.
+ * Roughly: the number of "bad" pixels is the number of pixel coordinates (x,y) where for (x,y)
+ * in image A, there is not a nearby similar pixel in image B *or* vice-versa (i.e. even if only
+ * one direction is bad, the pixel coordinate is bad).
+ *
+ * Example values: d=4, c=60. Color component values (RGBA) are 0-255.
+ *
+ * For each pixel at (x,y) in image A that is not within distance "d" from the edge of the image,
+ * we try to find a similar pixel within the square of pixels (of size 2"d") centered at (x,y) in
+ * image B. We also try the reverse (consider (x,y) in image B and search for a pixel in image A);
+ * if *either* search fails, this pixel coordinate is considered "bad". The result is the number
+ * of bad pixels.
+ * Given two pixels p and q, they are similar if they have similar color values; specifically, p
+ * and q are similar iff for *all* color components m in [R,B,G,A]:
+ * abs(p[m]-q[m]) <= "c"
+ *
+ * There are two reasons why we consider a pixel as bad if *either* direction fails (searching in
+ * image A or in image B):
+ * Reason 1: we probably want the comparison to be independent of image input order (commutative).
+ * Reason 2: given a reference image that is black, and a variant image that has gained
+ * a small patch of white pixels; the black pixels in the original image could all be
+ * matched to black pixels in the variant image, so we would not detect any difference.
+ * However, the white pixels in the variant image would not be matched to any pixels in
+ * the original image, so we would detect a different. Thus, comparing both directions and
+ * taking the worst case is typically more useful.
+ *
+ */
 public class FuzzyImageComparison {
 
+  /**
+   * See {@link FuzzyImageComparison}. Set of parameters used by the {@link FuzzyImageComparison}
+   * algorithm, plus the result if the algorithm completed (numDifferentResult).
+   */
   public static final class ThresholdConfiguration {
     public int componentThreshold;
     public int distanceThreshold;
+
+    /**
+     * The output from running the fuzzy image comparison with this configuration.
+     */
     public int numDifferentResult;
 
     public ThresholdConfiguration(int componentThreshold, int distanceThreshold) {
       this.componentThreshold = componentThreshold;
       this.distanceThreshold = distanceThreshold;
-      this.numDifferentResult = 0;
+      this.numDifferentResult = -1;
     }
   }
 
@@ -82,8 +122,7 @@ public class FuzzyImageComparison {
       int width,
       int height,
       int componentThreshold,
-      int distanceThreshold,
-      int[] outColorsBad) {
+      int distanceThreshold) {
 
     assert colorsLeft.length == colorsRight.length;
 
@@ -94,14 +133,9 @@ public class FuzzyImageComparison {
     for (int y = distanceThreshold; y < height - distanceThreshold; ++y) {
       for (int x = distanceThreshold; x < width - distanceThreshold; ++x) {
 
-        // Compare in both directions, and take the worst case (whichever order gives "different").
-        // Reason 1: we probably want the comparison to be independent of order (commutative).
-        // Reason 2: given a reference image that is black, and a variant image that has gained
-        // a small patch of white pixels; the black pixels in the original image could all be
-        // matched to black pixels in the variant image, so we would not detect any difference.
-        // However, the white pixels in the variant image would not be matched to any pixels in
-        // the original image, so we would detect a different. Thus, comparing both directions and
-        // taking the worst case is typically more useful.
+        // Given pixel in image A, find similar, nearby pixel in image B, and vice-versa.
+        // If either fails (hence || below), then the pixel coordinate is bad.
+        // See comments at top of file for justification.
         if (!doesSimilarNearPixelExist(
             colorsLeft,
             colorsRight,
@@ -120,8 +154,6 @@ public class FuzzyImageComparison {
             x,
             y)
         ) {
-          // We could include more information, but for now, if bad, we set to 1.
-          outColorsBad[y * height + x] = 1;
           ++numBad;
         }
       }
@@ -145,8 +177,6 @@ public class FuzzyImageComparison {
     int[] colorsRight = ImageColorComponents.getRgb(rightImage);
 
     for (ThresholdConfiguration thresholdConfiguration : thresholdConfigurations) {
-      // int[] values are 0 by default.
-      int[] badPixels = new int[colorsLeft.length];
 
       thresholdConfiguration.numDifferentResult = compareImageColors(
           colorsLeft,
@@ -154,20 +184,37 @@ public class FuzzyImageComparison {
           leftImage.getWidth(),
           leftImage.getHeight(),
           thresholdConfiguration.componentThreshold,
-          thresholdConfiguration.distanceThreshold,
-          badPixels);
+          thresholdConfiguration.distanceThreshold);
 
-      // TODO: Could post-process here (using badPixels array) to remove sparse bad pixels; i.e.
+      // TODO: Could post-process here (using a badPixels array) to remove sparse bad pixels; i.e.
       //  only clusters of bad pixels are considered bad.
     }
 
   }
 
+  /**
+   * Compares two images using the {@link FuzzyImageComparison} algorithm with some default
+   * configurations. Two default configurations are set by default, the first with a larger
+   * component threshold, and the second with a smaller component threshold.
+   *
+   * Typically, the first configuration has found a bad image if there were 10+ different pixels,
+   * and the second configuration has found a bad image if there were 10000+ different pixels.
+   * Thus, if either of the above is true, the image is bad.
+   *
+   * The justification is that the second configuration is too sensitive in most cases, but it
+   * can catch cases where an image has a *lot* of slightly different pixels, such as where the
+   * images are mostly different shades of grey.
+   *
+   * @param left first image file
+   * @param right second image file
+   * @return an array of configurations
+   * @throws IOException
+   */
   public static ThresholdConfiguration[] compareImagesDefault(
       File left,
       File right) throws IOException {
 
-    // Based on some sample images of 256x256:
+    // Default parameters based on some sample images of 256x256:
     // distance threshold: 4
     // component threshold: 60
     //   - diff of 10+ is probably bad
