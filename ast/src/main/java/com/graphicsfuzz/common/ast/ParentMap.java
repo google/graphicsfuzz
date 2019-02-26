ee25c35d6343e5d2c7a4b8a9a9b11022f433c2a3
Alastair Donaldson <afdx@google.com>
2019-02-26 18:33:51 +0000

    Changed BoolConstantExpr so that instead of having two static instanc… (#280)
    * Changed BoolConstantExpr so that instead of having two static instances, TRUE and FALSE, we create separate objects for each instance of 'true' and 'false' in the AST.  This fixes a bug where mutating 'true' in one place was leading to a 'true' in a different place being modified.
    
    * In the process, added a check during the building of a parent map to ensure that (except in documented special cases) a node does not have multiple parents.
    
    * Avoided traversing null ArrayInfo components of parameter declarations.
    
    * Refined CannedRandom so that spawning a child generator returns the CannedRandom instance (helpful for debugging).

--- ast/src/main/java/com/graphicsfuzz/common/ast/ParentMap.java
+++ ast/src/main/java/com/graphicsfuzz/common/ast/ParentMap.java
@@ -16,6 +16,8 @@
 
 package com.graphicsfuzz.common.ast;
 
+import com.graphicsfuzz.common.ast.decl.ArrayInfo;
+import com.graphicsfuzz.common.ast.type.Type;
 import com.graphicsfuzz.common.ast.visitors.StandardVisitor;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +46,13 @@ class ParentMap extends StandardVisitor implements IParentMap {
   protected <T extends IAstNode> void visitChildFromParent(Consumer<T> visitorMethod, T child,
       IAstNode parent) {
     super.visitChildFromParent(visitorMethod, child, parent);
+    // TODO(279): right now there are deliberately cases where a child can have a non-unique parent.
+    // We may want to reconsider this.
+    assert child instanceof Type
+        || child instanceof ArrayInfo
+        || !childToParent.containsKey(child) : "There should be no "
+        + "aliasing in the AST with the exception of types; found multiple parents for '" + child
+        + "' which has class " + child.getClass() + ".";
     childToParent.put(child, parent);
   }
 
