ecfcb4086e46921aeb68b233e13404676b4c7335
Alastair Donaldson <afdx@google.com>
2019-03-06 09:28:56 +0000

    Compute shader samples and documentation (#369)
    * Added additional compute shader samples, documentation on compute shaders, and example interestingness tests for compute shader reductions.

---
+++ python/src/main/python/drivers/inspect-compute-results.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 The GraphicsFuzz Project Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect_compute_results
+import sys
+
+try:
+    sys.exit(inspect_compute_results.main_helper(sys.argv[1:]))
+except ValueError as value_error:
+    sys.stderr.write(str(value_error))
+    sys.exit(1)
