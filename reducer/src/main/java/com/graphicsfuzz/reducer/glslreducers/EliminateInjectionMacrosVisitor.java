cf1b3e3da7b1bc7a5f29fce06c1894478dbf0084
Jiradeto <jiradet.jd@gmail.com>
2019-05-24 19:44:54 +0200

    Remove redundant functions

--- reducer/src/main/java/com/graphicsfuzz/reducer/glslreducers/EliminateInjectionMacrosVisitor.java
+++ reducer/src/main/java/com/graphicsfuzz/reducer/glslreducers/EliminateInjectionMacrosVisitor.java
@@ -17,115 +17,17 @@
 package com.graphicsfuzz.reducer.glslreducers;
 
 import com.graphicsfuzz.common.ast.IAstNode;
-import com.graphicsfuzz.common.ast.expr.ArrayIndexExpr;
 import com.graphicsfuzz.common.ast.expr.BinOp;
 import com.graphicsfuzz.common.ast.expr.BinaryExpr;
 import com.graphicsfuzz.common.ast.expr.ConstantExpr;
 import com.graphicsfuzz.common.ast.expr.Expr;
 import com.graphicsfuzz.common.ast.expr.FunctionCallExpr;
-import com.graphicsfuzz.common.ast.expr.MemberLookupExpr;
 import com.graphicsfuzz.common.ast.expr.ParenExpr;
-import com.graphicsfuzz.common.ast.expr.TernaryExpr;
-import com.graphicsfuzz.common.ast.expr.TypeConstructorExpr;
-import com.graphicsfuzz.common.ast.expr.UnaryExpr;
 import com.graphicsfuzz.common.ast.expr.VariableIdentifierExpr;
-import com.graphicsfuzz.common.ast.stmt.DoStmt;
-import com.graphicsfuzz.common.ast.stmt.ForStmt;
-import com.graphicsfuzz.common.ast.stmt.IfStmt;
-import com.graphicsfuzz.common.ast.stmt.LoopStmt;
-import com.graphicsfuzz.common.ast.stmt.SwitchStmt;
-import com.graphicsfuzz.common.ast.stmt.WhileStmt;
 import com.graphicsfuzz.common.ast.visitors.StandardVisitor;
 import com.graphicsfuzz.reducer.reductionopportunities.MacroNames;
 
 public class EliminateInjectionMacrosVisitor extends StandardVisitor {
-
-  @Override
-  public void visitBinaryExpr(BinaryExpr binaryExpr) {
-    super.visitBinaryExpr(binaryExpr);
-    cleanUpMacros(binaryExpr);
-  }
-
-  @Override
-  public void visitUnaryExpr(UnaryExpr unaryExpr) {
-    super.visitUnaryExpr(unaryExpr);
-    cleanUpMacros(unaryExpr);
-  }
-
-  @Override
-  public void visitParenExpr(ParenExpr parenExpr) {
-    super.visitParenExpr(parenExpr);
-    cleanUpMacros(parenExpr);
-  }
-
-  @Override
-  public void visitFunctionCallExpr(FunctionCallExpr functionCallExpr) {
-    super.visitFunctionCallExpr(functionCallExpr);
-    cleanUpMacros(functionCallExpr);
-  }
-
-  public void visitArrayIndexExpr(ArrayIndexExpr arrayIndexExpr) {
-    super.visitArrayIndexExpr(arrayIndexExpr);
-    cleanUpMacros(arrayIndexExpr);
-  }
-
-  public void visitMemberLookupExpr(MemberLookupExpr memberLookupExpr) {
-    super.visitMemberLookupExpr(memberLookupExpr);
-    cleanUpMacros(memberLookupExpr);
-  }
-
-  public void visitTernaryExpr(TernaryExpr ternaryExpr) {
-    super.visitTernaryExpr(ternaryExpr);
-    cleanUpMacros(ternaryExpr);
-  }
-
-  @Override
-  public void visitTypeConstructorExpr(TypeConstructorExpr typeConstructorExpr) {
-    super.visitTypeConstructorExpr(typeConstructorExpr);
-    cleanUpMacros(typeConstructorExpr);
-  }
-
-  @Override
-  public void visitIfStmt(IfStmt ifStmt) {
-    super.visitIfStmt(ifStmt);
-    if (MacroNames.isDeadByConstruction(ifStmt.getCondition())
-        || MacroNames.isIfWrapperFalse(ifStmt.getCondition())
-        || MacroNames.isIfWrapperTrue(ifStmt.getCondition())) {
-      ifStmt.setCondition(ifStmt.getCondition().getChild(0));
-    }
-  }
-
-  @Override
-  public void visitSwitchStmt(SwitchStmt switchStmt) {
-    super.visitSwitchStmt(switchStmt);
-    if (MacroNames.isSwitch(switchStmt.getExpr())) {
-      switchStmt.setExpr(switchStmt.getExpr().getChild(0));
-    }
-  }
-
-  @Override
-  public void visitWhileStmt(WhileStmt whileStmt) {
-    super.visitWhileStmt(whileStmt);
-    cleanupLoop(whileStmt);
-  }
-
-  private void cleanupLoop(LoopStmt loopStmt) {
-    if (MacroNames.isLoopWrapper(loopStmt.getCondition())) {
-      loopStmt.setCondition(loopStmt.getCondition().getChild(0));
-    }
-  }
-
-  @Override
-  public void visitDoStmt(DoStmt doStmt) {
-    super.visitDoStmt(doStmt);
-    cleanupLoop(doStmt);
-  }
-
-  @Override
-  public void visitForStmt(ForStmt forStmt) {
-    super.visitForStmt(forStmt);
-    cleanupLoop(forStmt);
-  }
 
   @Override
   protected void visitChildFromParent(IAstNode child, IAstNode parent) {
@@ -140,7 +42,12 @@ public class EliminateInjectionMacrosVisitor extends StandardVisitor {
         parent.replaceChild(child,
             addParenthesesIfNecessary(parent, ((FunctionCallExpr) child).getChild(1)));
       } else if (MacroNames.isFuzzed(expr)
-          || MacroNames.isDeadByConstruction(expr)) {
+          || MacroNames.isDeadByConstruction(expr)
+          || MacroNames.isSwitch(expr)
+          || MacroNames.isLoopWrapper(expr)
+          || MacroNames.isIfWrapperFalse(expr)
+          || MacroNames.isIfWrapperTrue(expr)
+      ) {
         parent.replaceChild(child,
             addParenthesesIfNecessary(parent, ((FunctionCallExpr) child).getChild(0)));
       }
@@ -189,27 +96,4 @@ public class EliminateInjectionMacrosVisitor extends StandardVisitor {
     return true;
   }
 
-  private void cleanUpMacros(Expr parent) {
-    for (int i = 0; i < parent.getNumChildren(); i++) {
-      Expr child = parent.getChild(i);
-      // Note: it would be redundant to have a special function reduction opportunity
-      // be classed also as a fuzzed expression reduction opportunity
-      if (MacroNames.isIdentity(child)
-          || MacroNames.isZero(child)
-          || MacroNames.isOne(child)
-          || MacroNames.isFalse(child)
-          || MacroNames.isTrue(child)) {
-        replaceChildWithGrandchild(parent, i, 1);
-      } else if (MacroNames.isFuzzed(child)
-          || MacroNames.isDeadByConstruction(child)) {
-        replaceChildWithGrandchild(parent, i, 0);
-      }
-    }
-  }
-
-  private void replaceChildWithGrandchild(Expr parent, int childIndex, int grandchildIndex) {
-    assert parent.getChild(childIndex).getNumChildren() > grandchildIndex;
-    parent.setChild(childIndex, (Expr) addParenthesesIfNecessary(parent,
-        parent.getChild(childIndex).getChild(grandchildIndex)));
-  }
 }
