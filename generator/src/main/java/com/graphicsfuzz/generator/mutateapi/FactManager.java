4024a6e379bff33d7aea83fb54fe307ad6a3299b
Jiradeto <jiradet.jd@gmail.com>
2019-08-08 15:21:06 +0700

    Address comments from code review for FactManager

--- generator/src/main/java/com/graphicsfuzz/generator/mutateapi/FactManager.java
+++ generator/src/main/java/com/graphicsfuzz/generator/mutateapi/FactManager.java
@@ -16,24 +16,16 @@
 
 package com.graphicsfuzz.generator.mutateapi;
 
-import com.graphicsfuzz.common.ast.decl.FunctionPrototype;
-import com.graphicsfuzz.common.ast.decl.VariableDeclInfo;
-import com.graphicsfuzz.common.ast.expr.Expr;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class FactManager {
 
   // A parent fact manager whose facts this fact manager extends.
   private final FactManager prototype;
-
   private final Map<Value, List<VariableFact>> variableFacts;
   private final Map<Value, List<FunctionFact>> functionFacts;
 
@@ -43,55 +35,83 @@ public class FactManager {
     functionFacts = new HashMap<Value, List<FunctionFact>>();
   }
 
+  /**
+   * Creates a new fact manager where this fact manager is used as a prototype. We basically call
+   * this method only when generating a new function where the return fact manager is non global.
+   *
+   * @return new fact manager.
+   */
   public FactManager newScope() {
     return new FactManager(this);
   }
 
-  public boolean isGlobalManager() {
+  /**
+   * Determines whether the fact manager is at the global scope.
+   *
+   * @return true if this manager has a prototype and false otherwise.
+   */
+  public boolean globalScope() {
     return prototype == null;
   }
 
-  public Map<Value, List<VariableFact>> getVariableFacts() {
-    return Collections.unmodifiableMap(variableFacts);
-  }
-
-  public Optional<List<VariableFact>> getVariableFacts(Value value) {
-    return variableFacts.entrySet()
-        .stream()
-        .filter(item -> item.getKey().equals(value))
-        .map(Map.Entry::getValue)
-        .findFirst();
-  }
-
-  public Map<Value, List<FunctionFact>> getFunctionFacts() {
-    return Collections.unmodifiableMap(functionFacts);
-  }
-
-  public Optional<List<FunctionFact>> getFunctionFacts(Value value) {
-    if (!functionFacts.containsKey(value)) {
-      return Optional.empty();
-    }
-    return functionFacts.entrySet()
-        .stream()
-        .filter(item -> item.getKey().equals(value))
-        .map(Map.Entry::getValue)
-        .findFirst();
-  }
-
+  /**
+   * Adds a new variable fact to the variable fact map of this fact manager.
+   *
+   * @param value        value which the given variable fact is representing.
+   * @param variableFact a new variable fact that will be added into the map of variable facts.
+   */
   public void addVariableFact(Value value, VariableFact variableFact) {
     final List<VariableFact> newFacts = variableFacts.getOrDefault(value, new ArrayList<>());
     newFacts.add(variableFact);
     variableFacts.put(value, newFacts);
   }
 
+  /**
+   * As function facts can only exist at the global scope, this method adds a new function
+   * fact into the map of the function facts of the global scope fact manager.
+   *
+   * @param value        value which the given function fact is representing.
+   * @param functionFact a new function fact that will be added into the map of function facts.
+   */
   public void addFunctionFact(Value value, FunctionFact functionFact) {
-    final List<FunctionFact> newFacts = functionFacts.getOrDefault(value, new ArrayList<>());
-    newFacts.add(functionFact);
-    functionFacts.put(value, newFacts);
-
-    if (prototype != null) {
+    if (globalScope()) {
+      final List<FunctionFact> newFacts = functionFacts.getOrDefault(value, new ArrayList<>());
+      newFacts.add(functionFact);
+      functionFacts.put(value, newFacts);
+    } else {
       prototype.addFunctionFact(value, functionFact);
     }
+  }
+
+  /**
+   * Retrieves a list of variable facts representing the value.
+   *
+   * @param value a value which the fact manager is asked to search in the variable fact map.
+   * @return if value does not exist in the variable fact map, an empty list is returned. Otherwise,
+   * returns a list of variable facts that guarantees to provide the given value.
+   */
+  public List<VariableFact> getVariableFacts(Value value) {
+    final List<VariableFact> result = new ArrayList<>();
+    result.addAll(variableFacts.getOrDefault(value, Collections.emptyList()));
+    if (!globalScope()) {
+      result.addAll(prototype.getVariableFacts(value));
+    }
+    return result;
+  }
+
+  /**
+   * Retrieves a list of function facts representing the value from the global scope fact
+   * manager. If the value does not exist in the map, an empty list is returned.
+   *
+   * @param value a value which the fact manager is asked to search in the function fact map.
+   * @return if value does not exist in the function fact map, an empty list is returned. Otherwise,
+   * returns a list of function facts that guarantees to provide the given value.
+   */
+  public List<FunctionFact> getFunctionFacts(Value value) {
+    if (globalScope()) {
+      return functionFacts.getOrDefault(value, Collections.emptyList());
+    }
+    return prototype.getFunctionFacts(value);
   }
 
 }
