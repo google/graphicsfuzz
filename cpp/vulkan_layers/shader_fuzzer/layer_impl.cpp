64b6430d38a0ff1b22e72a5ac4ae55151a60ca4d
Alastair Donaldson <afdx@google.com>
2019-11-14 19:41:00 -0800

    More work on layers.

--- cpp/vulkan_layers/shader_fuzzer/layer_impl.cpp
+++ cpp/vulkan_layers/shader_fuzzer/layer_impl.cpp
@@ -34,25 +34,43 @@
 
 namespace graphicsfuzz_shader_fuzzer {
 
-std::atomic_uint shader_counter(0);
+const char kPathSeparator =
+#ifdef _WIN32
+    '\\';
+#else
+    '/';
+#endif
 
+// Environment variable specifying where fuzzed shaders will be saved to.
+const char *kWorkDirEnvironmentVariable = "GRAPHICSFUZZ_SHADER_FUZZER_WORK_DIR";
+
+// Counter used as a source of shader module ids.
+std::atomic_uint shader_module_counter(0);
+
+// Returns an empty vector if fuzzing was not possible.  Otherwise, returns a
+// vector representing the fuzzed version of the shader referred to by
+// |pCreateInfo->pCode|.
 std::vector<uint32_t>
 TryFuzzingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
 
-  uint32_t shader_id = shader_counter++;
+  // Grab a new id for this shader module.
+  uint32_t shader_module_id = shader_module_counter++;
 
-  std::string work_dir_environment_variable =
-      "GRAPHICSFUZZ_SHADER_FUZZER_WORK_DIR";
-
-  const char *work_dir = std::getenv(work_dir_environment_variable.c_str());
+  // Check whether the work directory environment variable is set.
+  const char *work_dir = std::getenv(kWorkDirEnvironmentVariable);
   if (!work_dir) {
-    std::cerr << "Environment variable " << work_dir_environment_variable
+    std::cerr << "Environment variable " << kWorkDirEnvironmentVariable
               << " is not set; shaders will not be fuzzed." << std::endl;
     return std::vector<uint32_t>();
   }
+
+  // TODO(afd): The required target environment should be queried.
   const spv_target_env target_env = SPV_ENV_UNIVERSAL_1_3;
+
+  // |pCreateInfo->codeSize| gives the size in bytes; convert it to words.
   const uint32_t code_size_in_words =
       static_cast<uint32_t>(pCreateInfo->codeSize) / 4;
+
   spvtools::SpirvTools tools(target_env);
   if (!tools.IsValid()) {
     std::cerr << "Did not manage to create a SPIRV-Tools instance; shaders "
@@ -60,36 +78,54 @@ TryFuzzingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
               << std::endl;
     return std::vector<uint32_t>();
   }
+
+  // Create a fuzzer and the various parameters required for fuzzing.
   spvtools::fuzz::Fuzzer fuzzer(target_env);
   std::vector<uint32_t> binary_in(pCreateInfo->pCode,
                                   pCreateInfo->pCode + code_size_in_words);
   std::vector<uint32_t> result;
   spvtools::fuzz::protobufs::FactSequence no_facts;
   spvtools::fuzz::protobufs::TransformationSequence transformation_sequence;
-
   spvtools::FuzzerOptions fuzzer_options;
-  fuzzer_options.set_random_seed(shader_id);
+  fuzzer_options.set_random_seed(shader_module_id);
 
+  // Fuzz the shader into |result|.
   auto fuzzer_result_status = fuzzer.Run(binary_in, no_facts, fuzzer_options,
                                          &result, &transformation_sequence);
+
   if (fuzzer_result_status !=
       spvtools::fuzz::Fuzzer::FuzzerResultStatus::kComplete) {
     std::cerr << "Fuzzing failed." << std::endl;
     return std::vector<uint32_t>();
   }
 
-  // Write out the binary
+  // Write out the original shader module
   {
-    std::stringstream shader_binary_name;
-    shader_binary_name << "_fuzzed_" << shader_id << ".spv";
-    WriteFile<uint32_t>(shader_binary_name.str().c_str(), "wb", result.data(),
-                        result.size());
+    std::stringstream original_shader_binary_name;
+    original_shader_binary_name << std::string(work_dir) << kPathSeparator
+                                << "_original_" << shader_module_id
+                                << ""
+                                   ".spv";
+    WriteFile<uint32_t>(original_shader_binary_name.str().c_str(), "wb",
+                        pCreateInfo->pCode, code_size_in_words);
+  }
+
+  // Write out the fuzzed shader module
+  {
+    std::stringstream fuzzed_shader_binary_name;
+    fuzzed_shader_binary_name << std::string(work_dir) << kPathSeparator
+                              << "_fuzzed_" << shader_module_id
+                              << ""
+                                 ".spv";
+    WriteFile<uint32_t>(fuzzed_shader_binary_name.str().c_str(), "wb",
+                        result.data(), result.size());
   }
 
   // Write out the transformations
   {
     std::stringstream transformations_name;
-    transformations_name << "_fuzzed_" << shader_id << ".transformations";
+    transformations_name << std::string(work_dir) << kPathSeparator << "_"
+                         << shader_module_id << ".transformations";
     std::ofstream transformations_file;
     transformations_file.open(transformations_name.str(),
                               std::ios::out | std::ios::binary);
@@ -100,8 +136,8 @@ TryFuzzingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
   // Write out the transformations in JSON format
   {
     std::stringstream transformations_json_name;
-    transformations_json_name << "_fuzzed_" << shader_id
-                              << ".transformations_json";
+    transformations_json_name << std::string(work_dir) << kPathSeparator << "_"
+                              << shader_module_id << ".transformations_json";
     std::string json_string;
     auto json_options = google::protobuf::util::JsonOptions();
     json_options.add_whitespace = true;
@@ -121,10 +157,17 @@ VkResult vkCreateShaderModule(PFN_vkCreateShaderModule next, VkDevice device,
                               VkShaderModuleCreateInfo const *pCreateInfo,
                               AllocationCallbacks pAllocator,
                               VkShaderModule *pShaderModule) {
+  // Fuzzing the provided shader will either yield an empty vector - if
+  // something went wrong - or a vector whose contents is the fuzzed shader
+  // binary.
   std::vector<uint32_t> fuzzed = TryFuzzingShader(pCreateInfo);
+
   VkShaderModuleCreateInfo fuzzed_shader_module_create_info;
   VkShaderModuleCreateInfo const *fuzzed_shader_module_create_info_pointer;
   if (!fuzzed.empty()) {
+    // We succeeded in fuzzing the shader, so pass on a pointer to a new
+    // VkShaderModuleCreateInfo object identical to the original, except with
+    // the fuzzed shader data.
     fuzzed_shader_module_create_info_pointer =
         &fuzzed_shader_module_create_info;
     fuzzed_shader_module_create_info.sType = pCreateInfo->sType;
@@ -133,6 +176,8 @@ VkResult vkCreateShaderModule(PFN_vkCreateShaderModule next, VkDevice device,
     fuzzed_shader_module_create_info.codeSize = fuzzed.size() * 4;
     fuzzed_shader_module_create_info.pCode = fuzzed.data();
   } else {
+    // We did not succeed in fuzzing the shader, pass on the original
+    // VkShaderModuleCreateInfo pointer.
     fuzzed_shader_module_create_info_pointer = pCreateInfo;
   }
   return next(device, fuzzed_shader_module_create_info_pointer, pAllocator,
--- cpp/vulkan_layers/shader_scraper/layer_impl.cpp
+++ cpp/vulkan_layers/shader_scraper/layer_impl.cpp
@@ -19,7 +19,6 @@
 
 #include <atomic>
 #include <cstdlib>
-#include <fstream>
 #include <iostream>
 #include <memory>
 #include <sstream>
@@ -27,6 +26,7 @@
 #include "source/opt/build_module.h"
 #include "source/opt/ir_context.h"
 #include "spirv-tools/libspirv.hpp"
+#include "tools/io.h"
 
 namespace graphicsfuzz_shader_scraper {
 
@@ -37,24 +37,36 @@ const char kPathSeparator =
     '/';
 #endif
 
-std::atomic_uint shader_counter(0);
+// Environment variable specifying where shaders will be scraped to.
+const char *kWorkDirEnvironmentVariable =
+    "GRAPHICSFUZZ_SHADER_SCRAPER_WORK_DIR";
 
+// Counter used as a source of shader module ids.
+std::atomic_uint shader_module_counter(0);
+
+// Attempts to save out the shader module stored in |pCreateInfo->pCode|,
+// giving it an execution model-specific extension if it contains a single entry
+// point.
 void TryScrapingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
 
-  uint32_t shader_id = shader_counter++;
+  // Grab an id for the shader module.
+  uint32_t shader_module_id = shader_module_counter++;
 
-  std::string dump_dir_environment_variable =
-      "GRAPHICSFUZZ_SHADER_SCRAPER_DUMP_DIR";
-
-  const char *dump_dir = std::getenv(dump_dir_environment_variable.c_str());
-  if (!dump_dir) {
-    std::cerr << "Environment variable " << dump_dir_environment_variable
+  // Check whether the work directory environment variable is set.
+  const char *work_dir = std::getenv(kWorkDirEnvironmentVariable);
+  if (!work_dir) {
+    std::cerr << "Environment variable " << kWorkDirEnvironmentVariable
               << " is not set; shaders will not be scraped." << std::endl;
     return;
   }
+
+  // TODO(afd): The required target environment should be queried.
   const spv_target_env target_env = SPV_ENV_UNIVERSAL_1_3;
+
+  // |pCreateInfo->codeSize| gives the size in bytes; convert it to words.
   const uint32_t code_size_in_words =
       static_cast<uint32_t>(pCreateInfo->codeSize) / 4;
+
   spvtools::SpirvTools tools(target_env);
   if (!tools.IsValid()) {
     std::cerr << "Did not manage to create a SPIRV-Tools instance; shaders "
@@ -62,26 +74,50 @@ void TryScrapingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
               << std::endl;
     return;
   }
+
+  // Try to figure out a reasonable extension for the shader module, based on
+  // the entry point(s) it contains.
   std::string extension_prefix = "none";
   if (!tools.Validate(pCreateInfo->pCode, code_size_in_words)) {
+    // Save out the shader even if it is invalid, but indicate invalidity in
+    // the extension.
     extension_prefix = "invalid";
   } else {
+    // Parse the shader module so that we can inspect its entry point(s).
     std::unique_ptr<spvtools::opt::IRContext> ir_context =
         spvtools::BuildModule(target_env, nullptr, pCreateInfo->pCode,
                               code_size_in_words);
+
+    // Inspect at most two entry points in the shader module.
     for (auto &entry_point : ir_context->module()->entry_points()) {
       if (extension_prefix != "none") {
+        // We have seen an entry point already, so there must be multiple of
+        // them.  Record this in the extension, and stop looking at entry
+        // points.
         extension_prefix = "many";
         break;
       }
+      // Choose an extension based on the execution model of the entry point.
       switch (
           ir_context->module()->entry_points().begin()->GetSingleWordInOperand(
               0)) {
       case SpvExecutionModelFragment:
         extension_prefix = "frag";
         break;
+      case SpvExecutionModelGeometry:
+        extension_prefix = "geom";
+        break;
+      case SpvExecutionModelGLCompute:
+        extension_prefix = "comp";
+        break;
       case SpvExecutionModelVertex:
         extension_prefix = "vert";
+        break;
+      case SpvExecutionModelTessellationControl:
+        extension_prefix = "tesc";
+        break;
+      case SpvExecutionModelTessellationEvaluation:
+        extension_prefix = "tese";
         break;
       default:
         extension_prefix = "other";
@@ -89,13 +125,13 @@ void TryScrapingShader(VkShaderModuleCreateInfo const *pCreateInfo) {
       }
     }
   }
-  std::stringstream strstr;
-  strstr << std::string(dump_dir) << kPathSeparator << "_captured_shader_"
-         << shader_id << "." << extension_prefix << ".spv";
-  std::ofstream shader;
-  shader.open(strstr.str().c_str(), std::ios::out | std::ios::binary);
-  shader.write((const char *)pCreateInfo->pCode, pCreateInfo->codeSize);
-  shader.close();
+  // Write out the scraped shader module
+  std::stringstream shader_module_name;
+  shader_module_name << std::string(work_dir) << kPathSeparator
+                     << "_scraped_shader_" << shader_module_id << "."
+                     << extension_prefix << ".spv";
+  WriteFile<uint32_t>(shader_module_name.str().c_str(), "wb",
+                      pCreateInfo->pCode, code_size_in_words);
 }
 
 VkResult vkCreateShaderModule(PFN_vkCreateShaderModule next, VkDevice device,
