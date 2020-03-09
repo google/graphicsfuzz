3b95bb35db51639a76c37538d8580ebd262d1a45
Alastair Donaldson <afdx@google.com>
2020-03-09 12:28:36 +0000

    Add support for gl_FrontFacing builtin. (#900)
    This adds support for the gl_FrontFacing builtin in fragment shaders.

--- ast/src/main/java/com/graphicsfuzz/common/typing/Typer.java
+++ ast/src/main/java/com/graphicsfuzz/common/typing/Typer.java
@@ -567,6 +567,8 @@ public class Typer extends ScopeTrackingVisitor {
    */
   public static Optional<Type> maybeGetTypeOfBuiltinVariable(String name) {
     switch (name) {
+      case OpenGlConstants.GL_FRONT_FACING:
+        return Optional.of(BasicType.BOOL);
       case OpenGlConstants.GL_POINT_SIZE:
         return Optional.of(BasicType.FLOAT);
       case OpenGlConstants.GL_FRAG_COORD:
