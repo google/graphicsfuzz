9a1b5721ec7e9f724299412e52ca0b757ddb1868
Alastair Donaldson <afdx@google.com>
2020-03-24 12:45:31 +0000

    Fix name shadowing problem with switch statements
    When injecting switch statements, care was not being taken to account
    for variables declared in the original code being added to the scope
    used for fuzzing unreachable switch cases that occur after the
    reachable code.  This change fixes said problem, including some
    reworking of generation parameters that made the fix easier to test.

--- generator/src/main/java/com/graphicsfuzz/generator/fuzzer/Fuzzer.java
+++ generator/src/main/java/com/graphicsfuzz/generator/fuzzer/Fuzzer.java
@@ -62,11 +62,8 @@ import com.graphicsfuzz.generator.fuzzer.templates.VariableIdentifierExprTemplat
 import com.graphicsfuzz.generator.util.GenerationParams;
 import com.graphicsfuzz.util.Constants;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
