8be6aa2ced635129720d68eb77ae57a4e1e41257
jarisiru <53901722+jarisiru@users.noreply.github.com>
2020-03-30 15:08:25 +0300

    One variable per line and matrix support (#938)
    Split in/out variables to separate lines if more than one
    is declared on the same line, and also added support for
    matrix variables.

--- common/src/main/java/com/graphicsfuzz/common/util/FragmentShaderToShaderJob.java
+++ common/src/main/java/com/graphicsfuzz/common/util/FragmentShaderToShaderJob.java
@@ -24,10 +24,12 @@ import com.graphicsfuzz.common.ast.decl.VariableDeclInfo;
 import com.graphicsfuzz.common.ast.decl.VariablesDeclaration;
 import com.graphicsfuzz.common.ast.expr.BinOp;
 import com.graphicsfuzz.common.ast.expr.BinaryExpr;
+import com.graphicsfuzz.common.ast.expr.BoolConstantExpr;
 import com.graphicsfuzz.common.ast.expr.Expr;
 import com.graphicsfuzz.common.ast.expr.FloatConstantExpr;
 import com.graphicsfuzz.common.ast.expr.IntConstantExpr;
 import com.graphicsfuzz.common.ast.expr.TypeConstructorExpr;
+import com.graphicsfuzz.common.ast.expr.UIntConstantExpr;
 import com.graphicsfuzz.common.ast.expr.VariableIdentifierExpr;
 import com.graphicsfuzz.common.ast.stmt.BlockStmt;
 import com.graphicsfuzz.common.ast.stmt.ExprStmt;
@@ -117,82 +119,99 @@ public final class FragmentShaderToShaderJob {
     // Start layout locations from index 1
     int locationIndex = 1;
 
+
     for (VariablesDeclaration vd : tu.getGlobalVariablesDeclarations()) {
       if (vd.getBaseType().hasQualifier(TypeQualifier.SHADER_INPUT)) {
-        final BasicType basicType = (BasicType) vd.getBaseType().getWithoutQualifiers();
-        // Create clone of the variables declaration and change the input to output:
-        final QualifiedType varType = (QualifiedType) vd.getBaseType().clone();
-        varType.replaceQualifier(TypeQualifier.SHADER_INPUT, TypeQualifier.SHADER_OUTPUT);
-
-        // Set same layout location for vertex output and fragment input
-        ((QualifiedType) vd.getBaseType()).setLocationQualifier(locationIndex);
-        varType.setLocationQualifier(locationIndex);
-        locationIndex++;
-
-        // Check if this is the first input we're handling
-        if (!vertexShader.isPresent()) {
-          // Since we're going to need it, initialize vertexShader with
-          // skeleton definition
-          vertexShader = Optional.of(new TranslationUnit(ShaderKind.VERTEX,
-              Optional.of(ShadingLanguageVersion.ESSL_310), Arrays.asList(
-              new PrecisionDeclaration("precision mediump float;"),
-              new VariablesDeclaration(new QualifiedType(BasicType.VEC4,
-                  Arrays.asList(new LayoutQualifierSequence(
-                      new LocationLayoutQualifier(0)),
-                  TypeQualifier.SHADER_INPUT)),
-                  new VariableDeclInfo(Constants.GLF_POS, null, null)),
-              new FunctionDefinition(
-                  new FunctionPrototype("main", VoidType.VOID, Collections.emptyList()),
-                  new BlockStmt(Arrays.asList(
-                      new ExprStmt(new BinaryExpr(
-                      new VariableIdentifierExpr(OpenGlConstants.GL_POSITION),
-                      new VariableIdentifierExpr(Constants.GLF_POS),
-                      BinOp.ASSIGN))), false)))));
-        }
-
         for (VariableDeclInfo vdi : vd.getDeclInfos()) {
           if (vdi.hasArrayInfo()) {
             throw new RuntimeException("Arrays not implemented with " + vd.getBaseType().toString()
                 + " " + vdi.getName());
           }
+
+          final BasicType basicType = (BasicType) vd.getBaseType().getWithoutQualifiers();
+          // Create clone of the variables declaration and change the input to output:
+          final QualifiedType varTypeIn = (QualifiedType) vd.getBaseType().clone();
+          final QualifiedType varTypeOut = (QualifiedType) vd.getBaseType().clone();
+          varTypeOut.replaceQualifier(TypeQualifier.SHADER_INPUT, TypeQualifier.SHADER_OUTPUT);
+
+          // Set same layout location for vertex output and fragment input
+          varTypeIn.setLocationQualifier(locationIndex);
+          varTypeOut.setLocationQualifier(locationIndex);
+          // Increment location index
+          if (BasicType.allMatrixTypes().contains(basicType)) {
+            // For matrices, we'll skip ahead location indices to make sure
+            // there's no problem with them. Smaller matrices (like 2x3)
+            // don't need as much, but this makes testing easier.
+            locationIndex += 4;
+          } else {
+            locationIndex++;
+          }
+          // Check if this is the first input we're handling
+          if (!vertexShader.isPresent()) {
+            // Since we're going to need it, initialize vertexShader with
+            // skeleton definition
+            vertexShader = Optional.of(new TranslationUnit(ShaderKind.VERTEX,
+                Optional.of(ShadingLanguageVersion.ESSL_310), Arrays.asList(
+                new PrecisionDeclaration("precision mediump float;"),
+                new VariablesDeclaration(new QualifiedType(BasicType.VEC4,
+                    Arrays.asList(new LayoutQualifierSequence(
+                        new LocationLayoutQualifier(0)),
+                    TypeQualifier.SHADER_INPUT)),
+                    new VariableDeclInfo(Constants.GLF_POS, null, null)),
+                new FunctionDefinition(
+                    new FunctionPrototype("main", VoidType.VOID, Collections.emptyList()),
+                    new BlockStmt(Arrays.asList(
+                        new ExprStmt(new BinaryExpr(
+                        new VariableIdentifierExpr(OpenGlConstants.GL_POSITION),
+                        new VariableIdentifierExpr(Constants.GLF_POS),
+                        BinOp.ASSIGN))), false)))));
+          }
+
           // Add variables one by one, so the initialization code is more convenient
           vertexShader.get().addDeclarationBefore(
-              new VariablesDeclaration(varType, new VariableDeclInfo(vdi.getName(),
+              new VariablesDeclaration(varTypeOut, new VariableDeclInfo(vdi.getName(),
                   vdi.getArrayInfo(), null)),
               vertexShader.get().getMainFunction());
+          // Add variables one by one to the fragment shader too to get matching layout
+          tu.addDeclarationBefore(
+              new VariablesDeclaration(varTypeIn, new VariableDeclInfo(vdi.getName(),
+                  vdi.getArrayInfo(), null)),
+              vd);
           // Figure out the initializer
-          Expr initExpr = null;
-          if (basicType == BasicType.FLOAT) {
-            initExpr = new FloatConstantExpr(Float.toString(generator.nextFloat()));
-          } else if (basicType == BasicType.INT) {
-            initExpr = new IntConstantExpr(Integer.toString(generator.nextInt(0xffffff)));
-          } else if (basicType == BasicType.UINT) {
-            initExpr = new IntConstantExpr(Integer.toString(generator.nextPositiveInt(0xffffff)));
-          } else if (basicType == BasicType.VEC2) {
-            initExpr = new TypeConstructorExpr("vec2", Arrays.asList(
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat()))));
-          } else if (basicType == BasicType.VEC3) {
-            initExpr = new TypeConstructorExpr("vec3", Arrays.asList(
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat()))));
-          } else if (basicType == BasicType.VEC4) {
-            initExpr = new TypeConstructorExpr("vec4", Arrays.asList(
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat())),
-                new FloatConstantExpr(Float.toString(generator.nextFloat()))));
+          final List<Expr> constructorParams = new ArrayList<>();
+          if (basicType.getElementType() == BasicType.INT) {
+            for (int i = 0; i < basicType.getNumElements(); i++) {
+              constructorParams.add(new IntConstantExpr(Integer.toString(
+                  generator.nextInt(0xffffff))));
+            }
+          } else if (basicType.getElementType() == BasicType.UINT) {
+            for (int i = 0; i < basicType.getNumElements(); i++) {
+              constructorParams.add(new UIntConstantExpr(Integer.toUnsignedString(
+                  generator.nextPositiveInt(0xffffff)) + "u"));
+            }
+          } else if (basicType.getElementType() == BasicType.BOOL) {
+            for (int i = 0; i < basicType.getNumElements(); i++) {
+              constructorParams.add(new BoolConstantExpr(
+                  generator.nextBoolean()));
+            }
+          } else if (basicType.getElementType() == BasicType.FLOAT) {
+            for (int i = 0; i < basicType.getNumElements(); i++) {
+              constructorParams.add(new FloatConstantExpr(Float.toString(
+                  generator.nextFloat())));
+            }
           } else {
             throw new RuntimeException("Unimplemented variable type with "
                 + vd.getBaseType().toString() + " " + vdi.getName());
           }
+          final Expr initExpr = new TypeConstructorExpr(basicType.toString(), constructorParams);
           vertexShader.get().getMainFunction().getBody().insertStmt(0,
               new ExprStmt(new BinaryExpr(
                   new VariableIdentifierExpr(vdi.getName()),
                   initExpr,
                   BinOp.ASSIGN)));
         }
+        // Remove old variable declaration from fragment shader
+        tu.removeTopLevelDeclaration(vd);
       }
     }
 
--- common/src/test/java/com/graphicsfuzz/common/util/FragmentShaderToShaderJobTest.java
+++ common/src/test/java/com/graphicsfuzz/common/util/FragmentShaderToShaderJobTest.java
@@ -47,6 +47,30 @@ public class FragmentShaderToShaderJobTest {
     + "layout(location = 78) in vec3 in_v3;\n" // 78 expected to change to smaller value
     + "in vec4 in_v4;\n"
     + "\n"
+    + "in float in_a, in_b, in_c;\n"
+    + "in mat4 in_mat4;\n"
+    + "in mat3 in_mat3;\n"
+    + "in mat2 in_mat2;\n"
+    + "in mat2x3 in_mat2x3;\n"
+    + "in mat2x4 in_mat2x4;\n"
+    + "in mat3x2 in_mat3x2;\n"
+    + "in mat3x4 in_mat3x4;\n"
+    + "in mat4x2 in_mat4x2;\n"
+    + "in mat4x3 in_mat4x3;\n"
+    + "\n"
+    + "in bool in_bool;\n"
+    + "in bvec2 in_bvec2;\n"
+    + "in bvec3 in_bvec3;\n"
+    + "in bvec4 in_bvec4;\n"
+    + "in int in_int;\n"
+    + "in ivec2 in_ivec2;\n"
+    + "in ivec3 in_ivec3;\n"
+    + "in ivec4 in_ivec4;\n"
+    + "in uint in_uint;\n"
+    + "in uvec2 in_uvec2;\n"
+    + "in uvec3 in_uvec3;\n"
+    + "in uvec4 in_uvec4;\n"
+    + "\n"
     + "void main(void)\n"
     + "{\n"
     + "  " + Constants.GLF_COLOR + " = vec4(0.0, 0.0, 0.0, 1.0);\n"
@@ -94,6 +118,31 @@ public class FragmentShaderToShaderJobTest {
     assertTrue(vertdata.contains("layout(location = 2) out vec2 in_v2;"));
     assertTrue(vertdata.contains("layout(location = 3) out vec3 in_v3;"));
     assertTrue(vertdata.contains("layout(location = 4) out vec4 in_v4;"));
+    assertTrue(vertdata.contains("layout(location = 5) out float in_a;"));
+    assertTrue(vertdata.contains("layout(location = 6) out float in_b;"));
+    assertTrue(vertdata.contains("layout(location = 7) out float in_c;"));
+    assertTrue(vertdata.contains("layout(location = 8) out mat4 in_mat4;"));
+    assertTrue(vertdata.contains("layout(location = 12) out mat3 in_mat3;"));
+    assertTrue(vertdata.contains("layout(location = 16) out mat2 in_mat2;"));
+    assertTrue(vertdata.contains("layout(location = 20) out mat2x3 in_mat2x3;"));
+    assertTrue(vertdata.contains("layout(location = 24) out mat2x4 in_mat2x4;"));
+    assertTrue(vertdata.contains("layout(location = 28) out mat3x2 in_mat3x2;"));
+    assertTrue(vertdata.contains("layout(location = 32) out mat3x4 in_mat3x4;"));
+    assertTrue(vertdata.contains("layout(location = 36) out mat4x2 in_mat4x2;"));
+    assertTrue(vertdata.contains("layout(location = 40) out mat4x3 in_mat4x3;"));
+
+    assertTrue(vertdata.contains("layout(location = 44) out bool in_bool;"));
+    assertTrue(vertdata.contains("layout(location = 45) out bvec2 in_bvec2;"));
+    assertTrue(vertdata.contains("layout(location = 46) out bvec3 in_bvec3;"));
+    assertTrue(vertdata.contains("layout(location = 47) out bvec4 in_bvec4;"));
+    assertTrue(vertdata.contains("layout(location = 48) out int in_int;"));
+    assertTrue(vertdata.contains("layout(location = 49) out ivec2 in_ivec2;"));
+    assertTrue(vertdata.contains("layout(location = 50) out ivec3 in_ivec3;"));
+    assertTrue(vertdata.contains("layout(location = 51) out ivec4 in_ivec4;"));
+    assertTrue(vertdata.contains("layout(location = 52) out uint in_uint;"));
+    assertTrue(vertdata.contains("layout(location = 53) out uvec2 in_uvec2;"));
+    assertTrue(vertdata.contains("layout(location = 54) out uvec3 in_uvec3;"));
+    assertTrue(vertdata.contains("layout(location = 55) out uvec4 in_uvec4;"));
 
     assertTrue(vertdata.contains("layout(location = 0) in vec4 " + Constants.GLF_POS));
     assertTrue(vertdata.contains("gl_Position = " + Constants.GLF_POS));
@@ -102,6 +151,32 @@ public class FragmentShaderToShaderJobTest {
     assertTrue(fragdata.contains("layout(location = 2) in vec2 in_v2;"));
     assertTrue(fragdata.contains("layout(location = 3) in vec3 in_v3;"));
     assertTrue(fragdata.contains("layout(location = 4) in vec4 in_v4;"));
+
+    assertTrue(fragdata.contains("layout(location = 5) in float in_a;"));
+    assertTrue(fragdata.contains("layout(location = 6) in float in_b;"));
+    assertTrue(fragdata.contains("layout(location = 7) in float in_c;"));
+    assertTrue(fragdata.contains("layout(location = 8) in mat4 in_mat4;"));
+    assertTrue(fragdata.contains("layout(location = 12) in mat3 in_mat3;"));
+    assertTrue(fragdata.contains("layout(location = 16) in mat2 in_mat2;"));
+    assertTrue(fragdata.contains("layout(location = 20) in mat2x3 in_mat2x3;"));
+    assertTrue(fragdata.contains("layout(location = 24) in mat2x4 in_mat2x4;"));
+    assertTrue(fragdata.contains("layout(location = 28) in mat3x2 in_mat3x2;"));
+    assertTrue(fragdata.contains("layout(location = 32) in mat3x4 in_mat3x4;"));
+    assertTrue(fragdata.contains("layout(location = 36) in mat4x2 in_mat4x2;"));
+    assertTrue(fragdata.contains("layout(location = 40) in mat4x3 in_mat4x3;"));
+
+    assertTrue(fragdata.contains("layout(location = 44) in bool in_bool;"));
+    assertTrue(fragdata.contains("layout(location = 45) in bvec2 in_bvec2;"));
+    assertTrue(fragdata.contains("layout(location = 46) in bvec3 in_bvec3;"));
+    assertTrue(fragdata.contains("layout(location = 47) in bvec4 in_bvec4;"));
+    assertTrue(fragdata.contains("layout(location = 48) in int in_int;"));
+    assertTrue(fragdata.contains("layout(location = 49) in ivec2 in_ivec2;"));
+    assertTrue(fragdata.contains("layout(location = 50) in ivec3 in_ivec3;"));
+    assertTrue(fragdata.contains("layout(location = 51) in ivec4 in_ivec4;"));
+    assertTrue(fragdata.contains("layout(location = 52) in uint in_uint;"));
+    assertTrue(fragdata.contains("layout(location = 53) in uvec2 in_uvec2;"));
+    assertTrue(fragdata.contains("layout(location = 54) in uvec3 in_uvec3;"));
+    assertTrue(fragdata.contains("layout(location = 55) in uvec4 in_uvec4;"));
 
     assertTrue(fragdata.contains("layout(location = 0) out vec4 " + Constants.GLF_COLOR));
   }
