e98eb9782914378436bfac49ee6b06d88f6538ee
Nipun Garg <nipung271@gmail.com>
2020-06-06 15:00:18 +0530

    Rephrase the script comments to be repo-specific

--- experiments/build-Khronos_Samples_Vulkan.sh
+++ experiments/build-Khronos_Samples_Vulkan.sh
@@ -14,16 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Build one of the Vulkan demos Repo
-# {Sascha Willems, Google Test Applications, Khronos Samples, Imagination Demos}
-#
-# Run from the top level directory of the repo
-# Requires that cmake and the other dependencies 
-# for the given repo have to be fulfiled
-#
-# In particular, for the Sascha Willems demos, run
-# the download_assets.py script to download the
-# required assets before running this.
+# Build the Khronos Samples Repo
 
 set -e
 set -x
