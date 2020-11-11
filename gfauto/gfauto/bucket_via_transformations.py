51e560d6b4a3ed88493e1d33ebe74e41fa1509c9
Alastair Donaldson <afdx@google.com>
2020-11-11 16:40:27 +0000

    An idea.

--- gfauto/gfauto/bucket_via_transformations.py
+++ gfauto/gfauto/bucket_via_transformations.py
@@ -18,13 +18,15 @@
 
 import argparse
 import json
+import os
 import sys
 from pathlib import Path
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Tuple
 
 from gfauto import util
 from gfauto.gflogging import log
 from gfauto.util import check, check_dir_exists
+
 
 COMMON_TRANSFORMATION_TYPES = {
     "addConstantBoolean",
@@ -48,6 +50,7 @@ COMMON_TRANSFORMATION_TYPES = {
     "addTypeVector",
     "computeDataSynonymFactClosure",
     "splitBlock",
+    "replaceIdWithSynonym"
 }
 
 
@@ -77,13 +80,67 @@ def main() -> None:  # pylint: disable=too-many-locals;
         AssertionError(f"No summary dirs found under {str(tests_dir)}"),
     )
 
+    dir_to_transformations: Dict[str, Set[str]] = {}
+    interesting_transformations_to_dirs: List[Tuple[Set[str], List[str]]] = []
+    skipped_dirs: Set[str] = set()
+
+    max_len: int = 0
+    for summary_dir in summary_dirs:
+        log(f"Checking {summary_dir}")
+        transformations_json_file = summary_dir / "reduced_1" / "variant" / "shader.frag.transformations_json"
+        if not os.path.isfile(transformations_json_file):
+            continue
+        transformations_json = util.file_read_text(
+            transformations_json_file
+        )
+        transformations = json.loads(transformations_json)
+
+        transformation_types: Set[str] = set()
+
+        transformation_list = transformations["transformation"]
+
+        check(
+            bool(transformation_list),
+            AssertionError(f"No transformations found for {str(transformations_json)}"),
+        )
+
+        for transformation in transformation_list:
+            keys = transformation.keys()
+
+            check(
+                len(keys) == 1,
+                AssertionError(
+                    f"Transformation had more than one key: {transformation}"
+                ),
+            )
+            transformation_types.add(list(keys)[0])
+
+        transformation_types -= COMMON_TRANSFORMATION_TYPES
+        assert len(transformation_types) > 0
+        max_len = max(max_len, len(transformation_types))
+        dir_to_transformations[summary_dir] = transformation_types
+
+    seen_transformations: Set[str] = {'a'}
+    for i in range(1, max_len + 1):
+        for entry in dir_to_transformations:
+            transformations = dir_to_transformations[entry]
+            if len(transformations) == i:
+                common = seen_transformations.intersection(transformations)
+                if len(common) == 0:
+                    seen_transformations.update(transformations)
+                    print(str(entry) + ": " + "_".join(transformations))
+    """
+
     signature_to_dirs: Dict[str, List[Path]] = {}
     # For each summary directory, get its signature based on the transformation types remaining
     # and add the info to |signature_to_dirs|.
     for summary_dir in summary_dirs:
         log(f"Checking {summary_dir}")
+        transformations_json_file = summary_dir / "reduced_1" / "variant" / "shader.frag.transformations_json"
+        if not os.path.isfile(transformations_json_file):
+            continue
         transformations_json = util.file_read_text(
-            summary_dir / "reduced_1" / "variant" / "shader.frag.transformations_json"
+            transformations_json_file
         )
         transformations = json.loads(transformations_json)
 
@@ -136,7 +193,7 @@ def main() -> None:  # pylint: disable=too-many-locals;
         log(f"{signature}:")
         for case in cases:
             log(f"  {str(case)}")
-
+    """
 
 if __name__ == "__main__":
     main()
